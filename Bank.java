20976872c734e89019a64d9bdd534943aaa734d6
Mohit-Dhadiwal <158730570+Mohit-Dhadiwal@users.noreply.github.com>
2024-11-12 15:25:54 +0530

    Add files via upload
    Commit 2 Via Mohit

---
+++ Bank.java
@@ -0,0 +1,20 @@
+public class Bank{
+	String nameOfBank;
+	String ifscCode;
+	
+	String getNameOfBank(){
+		return nameOfBank;
+	}
+	
+	void setNameOfBank(String s){
+		nameOfBank = s;
+	}
+	
+	String getIfscCode(){
+		return ifscCode;
+	}
+	
+	void setIfscCode(String s){
+		ifscCode = s;
+	}
+}
---
+++ WIT.java
@@ -0,0 +1,20 @@
+public class WIT{
+	String className;
+	String classFloor;
+	
+	String getClassName(){
+		return className;
+	}
+	
+	void setClassName (String s){
+		className = s;
+	}
+	
+	String getClassFloor(){
+		return classFloor;
+	}
+	
+	void setClassFloor(String s){
+		classFloor = s;
+	}
+}
